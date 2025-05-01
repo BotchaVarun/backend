5474d6cf55926d6d2181935132b0436277d55fe7
BotchaVarun <varunbotcha@gmail.com>
2025-05-01 22:44:13 +0530

    m

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ console.log("Mongo URI from .env:", process.env.MONGO_URI);
 
 
 
-const cors = require("cors");
+
 
 const corsOptions = {
   origin: ["http://127.0.0.1:5500", "https://library-projects.vercel.app", "https://botchavarun.github.io/frontend/"],
